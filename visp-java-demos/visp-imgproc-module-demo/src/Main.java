32d1f52277164f69f00cde7bff6c5d54c636165a
Akshay Sharma <contact@imakshay.com>
2018-07-04 14:43:03 +0530

    simple Imgproc demo

---
+++ visp-java-demos/visp-imgproc-module-demo/src/Main.java
@@ -0,0 +1,37 @@
+import org.visp.core.VpImageRGBa;
+import org.visp.gui.VpDisplay;
+import org.visp.imgproc.VpContour;
+import org.visp.imgproc.VpDirection;
+import org.visp.imgproc.VpImgproc;
+import org.visp.io.VpImageIo;
+
+public class Main {
+	static {
+		System.loadLibrary("visp_java320");
+	}
+	
+	public static void main(String args[]) {
+		
+		// Simple contour demo
+		VpDirection direction = new VpDirection();
+		System.out.println(direction.clockwise().mDirectionType);
+		
+		VpContour contour = new VpContour(VpContour.VpContourType.CONTOUR_HOLE);
+		VpContour parent = new VpContour();
+		contour.setParent(parent);
+		
+		// Simple imgproc demo
+		VpImageRGBa image = new VpImageRGBa();
+		VpImageRGBa image_adjust = new VpImageRGBa();
+		VpImageIo.read(image, "monkey.png");
+		
+		double alpha = 1.5, beta = -10.0;
+		VpImgproc.adjust(image, image_adjust, alpha, beta);
+		
+		VpDisplay display = new VpDisplay();
+		display.display(image);
+
+		VpDisplay display2 = new VpDisplay();
+		display2.display(image_adjust);
+	}
+}

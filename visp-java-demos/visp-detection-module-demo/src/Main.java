f485dbe92bddd236d3000a6a71e7cf9935d45caf
Akshay Sharma <contact@imakshay.com>
2018-07-10 12:16:38 +0530

    complete demo with arrows

--- visp-java-demos/visp-detection-module-demo/src/Main.java
+++ visp-java-demos/visp-detection-module-demo/src/Main.java
@@ -1,7 +1,9 @@
 import org.visp.core.VpCameraParameters;
 import org.visp.core.VpHomogeneousMatrix;
+import org.visp.core.VpImageRGBa;
 import org.visp.core.VpImageUChar;
 import org.visp.detection.VpDetectorAprilTag;
+import org.visp.gui.VpDisplay;
 import org.visp.io.VpImageIo;
 
 import java.util.List;
@@ -13,7 +15,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		VpImageUChar imageUChar = new VpImageUChar();	
-		VpImageIo.read(imageUChar, "tagsampler.png");
+		VpImageIo.read(imageUChar, "sample.png");
+		VpImageRGBa colorImage = new VpImageRGBa();	
+		VpImageIo.read(colorImage, "sample.png");
 		
 		System.out.println(imageUChar.cols() + "x" + imageUChar.rows());
 		
@@ -22,16 +26,22 @@ public class Main {
 		boolean res = detectorAprilTag.detect(imageUChar);
 		System.out.println("Result was: "+res);
 		
-//		// Bogus detection
-//		VpImageIo.read(imageUChar, "monkey.png");
-//		res = detectorAprilTag.detect(imageUChar);
-//		System.out.println("Result was: "+res);
-		
 		// Detecting and get Projections
 		VpCameraParameters cam = new VpCameraParameters();
 		cam.initPersProjWithoutDistortion(615.1674805, 615.1675415, 312.1889954, 243.4373779);
 		List<VpHomogeneousMatrix> list =  detectorAprilTag.detect(imageUChar, 0.053, cam);
 		System.out.println("Size of list: "+list.size());
 		
+		VpDisplay display = new VpDisplay();
+		display.display(colorImage);
+		display.flush();
+		display.getClick();
+		
+		VpDisplay display2 = new VpDisplay();
+		display2.display(colorImage);
+		for(VpHomogeneousMatrix mat:list)
+			display2.displayFrame(mat, cam, 0.05, 3);
+		display2.flush();
+		display2.getClick();
 	}
 }
